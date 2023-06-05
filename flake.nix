23abf3c839f6d716f5d22b3b8bd129047ff5c218
Julie B. <no-reply@bbjubjub.fr>
2023-06-05 14:48:00 +0200

    nix: add clean-room derivation

--- flake.nix
+++ flake.nix
@@ -24,12 +24,23 @@
             rubber
           ];
         };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "report";
+          src = ./.;
+          buildInputs = with pkgs; [
+            texlive.combined.scheme-full
+            rubber
+          ];
+          installPhase = ''
+            install *.pdf -Dt $out
+          '';
+        };
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
         # agnostic ones like nixosModule and system-enumerating ones, although
         # those are more easily expressed in perSystem.
-
       };
     };
 }
